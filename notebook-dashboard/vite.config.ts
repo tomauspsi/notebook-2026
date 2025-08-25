02af0e597add6eceb37018c3068df06ab548c8e6
Tommaso Manari <tommaso.manari@gmail.com>
2025-08-25 15:36:36 +0200

    Add Pages deploy + update vite config + public copy

--- notebook-dashboard/vite.config.ts
+++ notebook-dashboard/vite.config.ts
@@ -1,7 +1,10 @@
+// notebook-dashboard/vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = process.env.BASE_URL || "/";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base
 });
