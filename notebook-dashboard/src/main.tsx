1355d7b5ef6a2ff3e52909b1a5d480395b80d6e9
tomauspsi <221123587+tomauspsi@users.noreply.github.com>
2025-08-30 11:30:44 +0200

    Update main.tsx

--- notebook-dashboard/src/main.tsx
+++ notebook-dashboard/src/main.tsx
@@ -4,8 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("app"); // <-- usa "app" come in index.html
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Elemento con id='app' non trovato in index.html");
+}
